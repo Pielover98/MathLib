16b7bc962aac3a20cfe87dbdd932aebad8f455c8
xavier melton <xavier melton>
2016-10-05 10:57:24 -0700

    Added more to controller

--- Components/SpaceShipController.cpp
+++ Components/SpaceShipController.cpp
@@ -3,6 +3,16 @@
 
 #include "sfwdraw.h"
 
+SpaceShipController::SpaceShipController(unsigned a_CTR_LEFT, unsigned a_CTR_RIGHT, unsigned a_CTR_UP, unsigned a_CTR_DOWN, unsigned a_CTR_BREAK)
+{
+	CTR_LEFT = a_CTR_LEFT;
+	CTR_RIGHT = a_CTR_RIGHT;
+	CTR_UP = a_CTR_UP;
+	CTR_DOWN = a_CTR_DOWN;
+	CTR_BREAK = a_CTR_BREAK;
+
+}
+
 void SpaceShipController::update(SpaceShipLocoMotion &loco)
 {
 	float hInput = 0.0f;
--- Components/SpaceShipController.h
+++ Components/SpaceShipController.h
@@ -3,5 +3,7 @@
 class SpaceShipController
 {
 public:
+	char CTR_LEFT, CTR_RIGHT, CTR_UP, CTR_DOWN, CTR_BREAK;
+	SpaceShipController(unsigned CTR_LEFT = 'A', unsigned CTR_RIGHT = 'D', unsigned CTR_UP = 'W', unsigned CTR_DOWN = 'S', unsigned CTR_BREAK = ' ');
 	void update(class SpaceShipLocoMotion &loco);
 };
